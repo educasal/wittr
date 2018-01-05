ba67e35c3b6eb589d1e7281559209933c977af1b
Eduardo Fernandez <eduardo.fernandez@mediasq.com>
2018-01-05 18:12:42 +0100

    Simplify the code

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -19,9 +19,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).then(function(response) {
       if (response.status === 404) {
-        return fetch('/imgs/dr-evil.gif').then(function(gifResponse){
-          return gifResponse;
-        });
+        return fetch('/imgs/dr-evil.gif');
       }
       return response;
     }).catch(function() {
