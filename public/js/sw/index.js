81ca3c860ea97735c796d972f6c07edad4d7662a
Eduardo Fernandez <eduardo.fernandez@mediasq.com>
2018-01-05 17:50:28 +0100

    Respond to requests with a url ending in ".jpg"

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,11 +1,18 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
+	// Ex1: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
-  event.respondWith(
-    new Response('You\'re <span class="a-winner-is-me">fucked</span>', {
-      headers: {'Content-Type': 'text/html'}
-    })
-  );
+  // event.respondWith(
+  //   new Response('You\'re <span class="a-winner-is-me">fucked</span>', {
+  //     headers: {'Content-Type': 'text/html'}
+  //   })
+  // );
+
+  // Ex2: only respond to requests with a
+  // url ending in ".jpg"
+  const url = event.request.url;
+  if (url.match(/\.jpg$/)) {
+    event.respondWith(fetch('/imgs/dr-evil.gif'));
+  }
+
 });
